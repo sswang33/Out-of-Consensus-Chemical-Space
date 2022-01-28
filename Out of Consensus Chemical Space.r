9b3b42a907f49f76fb687dda94a7a5e092b9781d
sswang33 <95211835+sswang33@users.noreply.github.com>
2022-01-28 16:43:28 +0800

    Update Out of Consensus Chemical Space.r

--- Out of Consensus Chemical Space.r
+++ Out of Consensus Chemical Space.r
@@ -8,12 +8,7 @@ rules <- list(
   list("include"=c(471),"exclude"=c(34,791,212,140))
 )
 dat_path <- args[1]
-dat <- scan(dat_path,what = c("character"),sep="\n")
-header <- unlist(stringr::str_split(dat[1],"\t"))
-dat <- as.data.frame(do.call("rbind",lapply(dat[-1],function(s){
-  unlist(stringr::str_split(s,"\t"))
-})),stringsAsFactors = F)
-colnames(dat) <- header
+dat <- read.table(dat_path,comment.char= "",sep="\t")
 dat$`SkinSensPred Score` <- as.numeric(dat$`SkinSensPred Score`)
 mols <- try(rcdk::parse.smiles(dat$CanonicalSMILES))
 if(class(mols)=="try-error"){
